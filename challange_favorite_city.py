3283449f32db91641ad48954e642620975f511f0
Ahmed M.Osman <64021350+akiid777@users.noreply.github.com>
2021-09-09 00:28:18 +0200

    Add files via upload

---
+++ challange_favorite_city.py
@@ -0,0 +1,6 @@
+def favorite_city(name_of_the_city):
+    print("one of my favorite cities is", name_of_the_city)
+
+favorite_city("Cairo")
+favorite_city("Alex")
+favorite_city("Marsa Matroh")
---
+++ challange_gussing game.py
@@ -0,0 +1,8 @@
+first_question = input("what's my favorite food?")
+
+if first_question == "fool":
+    print("Yep! So amazing!")
+else:
+    print("Yuck! thats not it!")
+
+print("Thanks for playing!")
